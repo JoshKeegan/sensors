8559620b78150f84a1c3a089447b46a3850a7fec
Josh Keegan <josh.keegan@gmx.com>
2021-04-03 17:20:20 +0100

    Working API reading co2!

--- server.py
+++ server.py
@@ -3,10 +3,14 @@ import mh_z19
 
 class Sensors:
     def on_get(self, req, resp):
-        co2Sensor = mh_z19.read_all()
+        # Pass serial_console_untouched as per instructions for running as non-root
+        # as the library expects to be running on native raspberry pi and have access to systemctl.
+        # See https://github.com/UedaTakeyuki/mh-z19/wiki/How-to-use-without-root-permission.
+        # Note that not all of the info there is relevant, it's not describing how to run in docker!
+        co2Sensor = mh_z19.read_all(True)
 
         sensors = {
-            "co2": co2Sensor.co2
+            "co2": co2Sensor["co2"]
         }
 
         resp.media = sensors
